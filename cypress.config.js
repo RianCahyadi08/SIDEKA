9eb39192174887356654d7ecfa2c3d5d88abc19e
LAPTOP-RRMTRDPL\Rian Cahyadi <ryancahyadi11@gmail.com>
2025-12-14 19:37:52 +0700

    First commit

---
+++ cypress.config.js
@@ -0,0 +1,19 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportPageTItle: "Report automation testing",
+    reportDir: "cypress/reports/mochawesome",
+    overwrite: true,
+    html: true,
+    json: true,
+    charts: true,
+    timestamp: "yyyy-mm-dd_HH-MM-ss",
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
